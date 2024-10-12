cc6d39b2ec32e33475d2735fed25bdaff5b90adf
ZaRdEr15 <73446410+ZaRdEr15@users.noreply.github.com>
2024-10-12 12:42:02 +0300

    Robot moves until reaches final pos

--- main.gd
+++ main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var plate = preload("res://plate.tscn")
+@onready var plate = preload("res://plate/plate.tscn")
 var event = 'none'
 
 # Called when the node enters the scene tree for the first time.
@@ -12,7 +12,7 @@ func _input(event):
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			print("Click ", event.position)
 			event = "click"
-		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
+		elif event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
 			print("Wheel up")
 			event = "wheel up"
 
--- robot/robot.gd
+++ robot/robot.gd
@@ -1,49 +1,30 @@
 extends Area2D
 
 
-# For testing
-const LVL_MOVEMENT: Array[Vector2] = [
-	Vector2.DOWN,
-	Vector2.DOWN,
-	Vector2.RIGHT,
-	Vector2.RIGHT,
-	Vector2.UP, 
-	Vector2.UP,
-	Vector2.LEFT,
-	Vector2.LEFT,
-]
-var final_direction_idx := LVL_MOVEMENT.size() # for testing
-var current_direction_idx: int = 0
-
-
-
 const TILE_SIZE: int = 16
 var current_direction := Vector2.DOWN
 @onready var ray = $RayCast2D
 @onready var final_position := position + current_direction * TILE_SIZE
 var can_move := true
-
+var start_movement := false
 
 func _ready() -> void:
 	pass
 
 
 func _process(delta: float) -> void:
-	if can_move:
+	if can_move and start_movement:
 		position = position.move_toward(final_position, 0.5)
+		if position == final_position:
+			$Timer.start(1)
+			start_movement = false
 
 
 func _on_timer_timeout() -> void:
+	start_movement = true
 	final_position = position + current_direction * TILE_SIZE
 	ray.force_raycast_update()
 	ray.set_target_position(current_direction * TILE_SIZE)
 	#choose_next_direction()
 	if ray.is_colliding():
 			can_move = false
-	
-
-func choose_next_direction() -> void:
-	current_direction = LVL_MOVEMENT[current_direction_idx]
-	current_direction_idx += 1
-	if current_direction_idx == final_direction_idx:
-		current_direction_idx = 0
