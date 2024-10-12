8f48da3fcdbbd84dab643ed9ee69856712b80d0f
grubeljas <p.grubel1as@gmail.com>
2024-10-12 16:01:36 +0300

    inventory for plates UI

---
+++ inv_ui/inv_ui_slot.gd
@@ -0,0 +1,10 @@
+extends Panel
+
+@onready var item_visual: Sprite2D = $CenterContainer/Panel/item_display
+
+func update(slot: InvSlot):
+	if !slot:
+		item_visual.visible = false
+	else:
+		item_visual.visible = true
+		item_visual.texture = slot.item.texture
