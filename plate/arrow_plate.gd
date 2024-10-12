7c1077513c5b2ee859a719e9e3da89ef5213ca8d
ZaRdEr15 <73446410+ZaRdEr15@users.noreply.github.com>
2024-10-12 19:46:51 +0300

    Idle, walking, death animations and reset

--- plate/arrow_plate.gd
+++ plate/arrow_plate.gd
@@ -1,9 +1,8 @@
-class_name Plate extends Area2D
+extends BasePlate
 
 
 @onready var raycast = $RayCast2D
 @onready var next_direction = (raycast.to_global(raycast.target_position) - raycast.to_global(Vector2.ZERO)).normalized()
-var can_rotate := false
 
 
 func _input(event: InputEvent) -> void:
@@ -15,15 +14,6 @@ func _input(event: InputEvent) -> void:
 				next_direction = (raycast.to_global(raycast.target_position) - raycast.to_global(Vector2.ZERO)).normalized()
 
 
-
 func _on_area_entered(area: Area2D) -> void:
 	area.current_direction = next_direction
 	area.ray.set_target_position(area.current_direction * area.TILE_SIZE)
-
-
-func _on_mouse_entered() -> void:
-	can_rotate = true
-
-
-func _on_mouse_exited() -> void:
-	can_rotate = false
