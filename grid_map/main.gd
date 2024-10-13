b10e62c3ce61cf08946e380a0b976a41b3b57be3
grubeljas <p.grubel1as@gmail.com>
2024-10-13 14:33:17 +0300

    place items

--- grid_map/main.gd
+++ grid_map/main.gd
@@ -4,6 +4,8 @@ var mouse_input = 'none'
 @export var items = [["Back", 2], ["Arrow", 5]]
 @onready var inv_ui = $CanvasLayer/Inv_UI
 var dict = PlateDict
+@export var selected_item: BasePlate
+@export var item_to_place: BasePlate
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,20 +17,30 @@ func parse_items():
 		new_items.append([dict.inv_items.get(item[0]), item[1]])
 	inv_ui.insert_into_slots(new_items)
 	
-
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if Input.is_action_just_pressed("click") and event.pressed:
 			select()
 		elif event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			print("Wheel up")
 			mouse_input = "wheel up"
 
 func select():
-	var mouse_pos = $TileMapLayer2.local_to_map(get_local_mouse_position())
-	if $TileMapLayer.get_cell_tile_data(mouse_pos):
-		var pos = $TileMapLayer2.map_to_local(mouse_pos)
+	if selected_item:
+		item_to_place = selected_item.duplicate()
+		var mouse_pos = $FrameTileMapLayer.local_to_map(get_local_mouse_position())
+		if $TileMapLayer.get_cell_tile_data(mouse_pos):
+			print(item_to_place)
+			var pos = $FrameTileMapLayer.map_to_local(mouse_pos)
+			item_to_place.position = pos
+			item_to_place.visible = true
+			add_child(item_to_place)
+		
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_inv_ui_item_in_mouse(item: String) -> void:
+	selected_item = dict.plate_nodes.get(item).instantiate()
+	print(typeof(selected_item))
--- grid_map/tile_map_layer_2.gd
+++ grid_map/tile_map_layer_2.gd
@@ -1,6 +1,8 @@
 extends TileMapLayer
 
 var mouse_pos: Vector2i = Vector2i(-1, -1)
+@onready var level = $"../TileMapLayer"
+signal can_place
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,4 +13,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	erase_cell(mouse_pos)
 	mouse_pos = local_to_map(get_local_mouse_position())
-	set_cell(mouse_pos, 0, Vector2i(0, 0), 0)
+	var level_tile: TileData = level.get_cell_tile_data(mouse_pos)
+	if level_tile and level_tile.get_custom_data("can_place"):
+		set_cell(mouse_pos, 0, Vector2i(0, 0), 0)
+		can_place.emit()
